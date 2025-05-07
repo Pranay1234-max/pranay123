4d6df8e06109523705d7faab6754a5a64198fb1e
Pranay1234-max <pranaydighe70@gmail.com>
2025-05-07 12:21:03 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,2 +1,8 @@
-print('hello')
-print('raj')
+def add_numbers(num1, num2):
+    return num1 + num2
+
+
+number1 = 5
+number2 = 3
+result = add_numbers(number1, number2)
+print(f"The sum of {number1} and {number2} is {result}")
